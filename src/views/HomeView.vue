40e554ce47bbf4904efba83204c2a2f842b7853c
ajyydz <2238662003@qq.com>
2026-05-16 16:32:44 +0800

    style: 加深页面和头部导航渐变颜色

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@ import SideNav from '../components/SideNav.vue'
 </script>
 
 <template>
-  <div class="flex min-h-screen bg-gradient-to-br from-gray-50 via-white to-blue-50 dark:from-gray-900 dark:via-gray-800 dark:to-gray-900">
+  <div class="flex min-h-screen bg-gradient-to-br from-blue-100 via-white to-cyan-100 dark:from-gray-900 dark:via-gray-800 dark:to-blue-900">
     <!-- 左侧导航 -->
     <SideNav />
     
